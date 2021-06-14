fefb81907de5e168d06f4d0cabde3c10755c371b
harsh <harshpatel5404@gmail.com>
2021-06-14 20:43:42 +0530

    first

--- lib/home.dart
+++ lib/home.dart
@@ -1,52 +1,95 @@
 import 'package:flutter/material.dart';
- 
+import 'package:google_fonts/google_fonts.dart';
+
 void main() => runApp(MyHome());
- 
+
 class MyHome extends StatefulWidget {
   @override
   _MyHomeState createState() => _MyHomeState();
 }
 
 class _MyHomeState extends State<MyHome> {
-  Widget quizcard(){
+  Widget quizcard(String lang, String img) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Card(
-        elevation: 10,
-        child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            onTap: () {
-              print('Card tapped.');
-            },
-            child: Container(
-              padding: EdgeInsets.all(20),
-              height: MediaQuery.of(context).size.height/3,
-              width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(15),
+      child: InkWell(
+        child: Material(
+          color: Colors.indigo,
+          elevation: 10,
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.circular(100),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      child: ClipOval(
+                        child: Image(
+                          image: AssetImage("./assets/${img}"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    lang,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "Hii as sdfgf wer x csfd erna qwdghasr jfsoiosdn nsd asdg hello my name usb folng weight vol.sdn nsd asdg hello my name usb folng as sdfgf wer x csfd erna qwdghasr jfsoiosdn nsd asdg hello my name usb folng weight vol.sdn nsd asdg hello my name usb folng weight volas fd.",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.justify,
+                    maxLines: 5,
+                  ),
+                ),
+              ],
             ),
           ),
+        ),
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
+    List img = [
+      "images/cpp.png",
+      "images/java.png",
+      "images/js.png",
+      "images/py.jpg",
+      "images/ruby.jpg",
+    ];
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Quiz App'),
-        ),
-        body:Container(
-        child: Column(
+          appBar: AppBar(
+            title: Text('Quiz App'),
+          ),
+          body: ListView(
             children: [
-              Row(
-                children: [
-                  quizcard()
-                ],
-              )
+              quizcard("C++", img[0]),
+              quizcard("Java", img[1]),
+              quizcard("Java Script", img[2]),
+              quizcard("Python", img[3]),
+              quizcard("Ruby", img[4]),
             ],
-        ),
-    ),
-      ),
+          )),
     );
   }
 }

aa889dfaea38bdf586bb6320ddb2aefbb02fef1e
harsh <harshpatel5404@gmail.com>
2021-06-15 10:41:12 +0530

    normal

--- lib/home.dart
+++ lib/home.dart
@@ -95,7 +95,7 @@ class _MyHomeState extends State<MyHome> {
     return Scaffold(
           appBar: AppBar(
             // backgroundColor: Color.fromARGB(30, 0, 255, 255),
-            title: Text('Quizzz App'),
+            title: Text('Quiz App'),
           ),
           body: ListView(
             children: [
